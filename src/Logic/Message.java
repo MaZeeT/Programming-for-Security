d8eecb7ea8d2ce596093af87ca63bee82d853d9c
MaZeeT <30165307+MaZeeT@users.noreply.github.com>
2020-02-24 09:08:51 +0100

    Added username to a message

--- src/Logic/Message.java
+++ src/Logic/Message.java
@@ -3,16 +3,23 @@ package Logic;
 import java.io.Serializable;
 
 public class Message implements Serializable {
+    private String username;
     private String message;
     private int iv;
 
-    public Message(String message) {
+    public Message(String username, String message) {
+        this.username = username;
         this.message = message;
     }
 
-    public Message(String message, int iv) {
+    public Message(String username, String message, int iv) {
+        this.username = username;
         this.message = message;
         this.iv = iv;
+    }
+
+    public String getUsername(){
+        return username;
     }
 
     public String getMessage() {
