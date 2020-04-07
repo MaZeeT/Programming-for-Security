1cdac61b40e9e1d0075fa318a0a319136e70e3e2
MaZeeT <30165307+MaZeeT@users.noreply.github.com>
2020-04-07 11:31:47 +0200

    Update AESEncryption.java

--- src/Logic/AESEncryption.java
+++ src/Logic/AESEncryption.java
@@ -6,12 +6,12 @@ import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
-public class SymmetricEncryption {
+public class AESEncryption {
     SecretKeySpec key;
     byte[] iv;
     Cipher cipher;
 
-    public SymmetricEncryption(SecretKeySpec key, String iv) {
+    public AESEncryption(SecretKeySpec key, String iv) {
         this.key = key;
         this.iv = Hex.decode(iv);
         try {
