146c5d32d2dadd53d773e0b9dc2b5b96c4b4bf04
MaZeeT <30165307+MaZeeT@users.noreply.github.com>
2020-04-07 11:55:52 +0200

    Update MessageTest.java

--- test/Network/MessageTest.java
+++ test/Network/MessageTest.java
@@ -2,6 +2,8 @@ package Network;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MessageTest {
@@ -13,10 +15,10 @@ class MessageTest {
         Message message = new Message(username, plaintext);
 
         byte[] signature = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
-
         boolean firstSign = message.signMessage(signature);
 
         assertTrue(firstSign);
+        assertArrayEquals(message.getSignature(), signature);
     }
 
     @Test
@@ -25,13 +27,15 @@ class MessageTest {
         String plaintext = "testing message";
         Message message = new Message(username, plaintext);
 
-        byte[] signature = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
-
-        boolean firstSign = message.signMessage(signature);
-        boolean secondSign = message.signMessage(signature);
+        byte[] signatureA = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+        byte[] signatureB = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        boolean firstSign = message.signMessage(signatureA);
+        boolean secondSign = message.signMessage(signatureB);
 
         assertTrue(firstSign);
         assertFalse(secondSign);
+        assertTrue(Arrays.equals(message.getSignature(),signatureA));
+        assertFalse(Arrays.equals(message.getSignature(),signatureB));
     }
 
     @Test
