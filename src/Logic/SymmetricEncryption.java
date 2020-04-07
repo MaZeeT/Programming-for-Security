ba4cd5491d6059dffae1f36888f0f2b65d7e9a37
MaZeeT <30165307+MaZeeT@users.noreply.github.com>
2020-04-07 11:30:47 +0200

    Update SymmetricEncryption.java

--- src/Logic/SymmetricEncryption.java
+++ src/Logic/SymmetricEncryption.java
@@ -6,12 +6,12 @@ import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
-public class Symmetric {
+public class SymmetricEncryption {
     SecretKeySpec key;
     byte[] iv;
     Cipher cipher;
 
-    public Symmetric(SecretKeySpec key, String iv) {
+    public SymmetricEncryption(SecretKeySpec key, String iv) {
         this.key = key;
         this.iv = Hex.decode(iv);
         try {
