738a64b664217497b0d8b6f990a12939a97edf7a
MaZeeT <30165307+MaZeeT@users.noreply.github.com>
2020-04-07 11:29:08 +0200

    refactored symmetric encryption class

---
+++ src/Logic/Symmetric.java
@@ -0,0 +1,44 @@
+package Logic;
+
+import org.bouncycastle.util.encoders.Hex;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+public class Symmetric {
+    SecretKeySpec key;
+    byte[] iv;
+    Cipher cipher;
+
+    public Symmetric(SecretKeySpec key, String iv) {
+        this.key = key;
+        this.iv = Hex.decode(iv);
+        try {
+            this.cipher = Cipher.getInstance("AES/CBC/PKCS5Padding", "BC");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public byte[] Encrypt(byte[] input) {
+        try {
+            cipher.init(Cipher.ENCRYPT_MODE, key, new IvParameterSpec(iv));
+            return cipher.doFinal(input);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public byte[] Decrypt(byte[] input){
+        try {
+            cipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(iv));
+            return cipher.doFinal(input);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+}
