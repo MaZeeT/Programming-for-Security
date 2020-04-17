e2c7fcea9284e882a7dc9025cc049dc6104fd3c2
MaZeeT <30165307+MaZeeT@users.noreply.github.com>
2020-04-17 22:51:53 +0200

    added encryption and signatures to the messages

--- src/Client/ChatClient.java
+++ src/Client/ChatClient.java
@@ -12,30 +12,23 @@ import Network.Message;
 import java.io.IOException;
 import java.net.Socket;
 import java.security.interfaces.RSAPublicKey;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
 
 public class ChatClient implements EventSubscriber<Message> {
     ChatConnection chatConnection;
     AESEncryption aesEncryption;
     KeyMaster keyMaster;
-    List<Message> messageHistory;
 
     public ChatClient(String ip, int port, KeyMaster keyMaster) throws IOException {
-        chatConnection = new ChatConnection(new Socket(ip, port));
-        aesEncryption = new AESEncryption(keyMaster.symmetricKey());
+        this.chatConnection = new ChatConnection(new Socket(ip, port));
+        this.aesEncryption = new AESEncryption(keyMaster.symmetricKey());
         this.keyMaster = keyMaster;
-
-        messageHistory = new LinkedList<>();
         EventNotifier.messageSent.subscribe(this);
         startReceiving();
     }
 
 
     private void sendMessage(Message message) {
-        System.out.println("Signed Message: " + SignatureSigner.sign(message, keyMaster.asymmetricKeyPair()));
-
+        SignatureSigner.sign(message, keyMaster.asymmetricKeyPair());
         CipherMessage cMessage = aesEncryption.Encrypt(message);
         chatConnection.send(cMessage);
     }
@@ -51,39 +44,23 @@ public class ChatClient implements EventSubscriber<Message> {
         }).start();
     }
 
+    //CipherMessage is encrypted messages from clients typical signed, where a Message is message from the server not signed.
     private void announceInput(Object input) {
         if (input instanceof CipherMessage) {
             CipherMessage cMessage = (CipherMessage) input;
             Message message = aesEncryption.Decrypt(cMessage);
             if (verified(message)) {
                 EventNotifier.messageReceived.publishEvent(message);
-                System.out.println(Arrays.toString(cMessage.signature()));
-                messageHistory.add(message);
             }
-
         } else if (input instanceof Message) {
             Message message = (Message) input;
             EventNotifier.messageReceived.publishEvent(message);
-            messageHistory.add(message);
         }
     }
 
     private boolean verified(Message message) {
         RSAPublicKey publicKey = keyMaster.publicKeyOf(message.username());
-
-        boolean result = SignatureSigner.verify(message, publicKey);
-        System.out.println("is verifing - " + result);
-
-        return result;
-
-    }
-
-    public void stopReceiving() {
-        isReceiving = false;
-    }
-
-    public List<Message> getMessageHistory() {
-        return messageHistory;
+        return SignatureSigner.verify(message, publicKey);
     }
 
     @Override
