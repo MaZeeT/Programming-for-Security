a4e744257b0b1c19b0c6773c3dd69bb6f7c6619c
MaZeeT <30165307+MaZeeT@users.noreply.github.com>
2020-04-18 00:13:20 +0200

    Update Main.java

--- src/Client/Main.java
+++ src/Client/Main.java
@@ -8,7 +8,6 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 import javax.crypto.spec.SecretKeySpec;
-import java.io.IOException;
 import java.security.KeyPair;
 import java.security.interfaces.RSAPublicKey;
 
@@ -18,26 +17,22 @@ public class Main extends Application {
         launch(args);
     }
 
+    char[] password = "HardcodedPasswordIsBad".toCharArray();
+    byte[] salt = {0, 1, 1, 1, 2, 0, 6, 3};
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         String ip = "127.0.0.1";
         int port = 3000;
 
-        char[] password = "HardcodedPasswordIsBad".toCharArray();
-        byte[] salt = {0, 1, 1, 1, 2, 0, 6, 3};
-
-        SecretKeySpec secretKey = KeyMaster.generateSecretKey(password, salt);
-        KeyPair keyPair = KeyMaster.generateKeyPair();
-        KeyMaster keyMaster = new KeyMaster(secretKey, keyPair);
-
         String username = "ClientTerminal";
-        ChatClient client = launchClient(ip, port, keyMaster, username);
+        ChatClient client = launchClient(ip, port, username);
         UI ui = launchTerminal(username);
         UI gui = launchGUI(primaryStage, username);
     }
 
     private UI launchGUI(Stage primaryStage, String username) {
-        ChatGUI gui = new ChatGUI(username,800, 600);
+        ChatGUI gui = new ChatGUI(username, 800, 600);
         primaryStage.setTitle("ChatCrypt");
         primaryStage.setScene(gui.getScene());
         primaryStage.show();
@@ -48,9 +43,16 @@ public class Main extends Application {
         return new TerminalUI(username);
     }
 
-    private ChatClient launchClient(String ip, int port, KeyMaster keyMaster, String username) throws IOException {
+    private ChatClient launchClient(String ip, int port, String username) throws Exception {
+        //generate keys and combine into the KeyMaster class
+        SecretKeySpec secretKey = KeyMaster.generateSecretKey(password, salt);
+        KeyPair keyPair = KeyMaster.generateKeyPair();
+        KeyMaster keyMaster = new KeyMaster(secretKey, keyPair);
+
+        //todo announce public key to server and keep a collection of public keys.
         RSAPublicKey publicKey = (RSAPublicKey) keyMaster.asymmetricKeyPair().getPublic();
         keyMaster.addPublicKey(username, publicKey);
+
         return new ChatClient(ip, port, keyMaster);
     }
 
